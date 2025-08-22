c2abc09bf811a84f28e7949563b91d6cf561c8e8
RamonDuzanDev <271955@unifio.edu.br>
2025-08-22 01:36:44 +0000

    Atualizacao de classes

--- src/main/java/app/PrimeiraClasse.java
+++ src/main/java/app/PrimeiraClasse.java
@@ -2,6 +2,13 @@ package app;
 
 public class PrimeiraClasse {
     public static void main(String[] args) {
-        System.out.println("Olá Gradle!!!");
+        Aluno a1 = new Aluno();
+        a1.setNome("Ramon");
+        a1.setIdade(19);
+
+        System.out.println(a1.getNome());
+        System.out.println(a1.getIdade());
+        
+        System.out.println("Olá, Gradle!!!");
     }
 }
